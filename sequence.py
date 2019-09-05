5c4a856be46d83bceebf3e262f4ff77ba852581e
Matthias <matthiasg19@ru.is>
2019-09-05 10:46:12 +0000

    Sequence completed

--- sequence.py
+++ sequence.py
@@ -1,4 +1,17 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+first = 3
+second = 2
+third = 1
 
 for i in range(n):
-    
+    if i <= 2:
+        print(i+1)
+    else:
+        print(first + second + third)
+        temp_1 = first
+        temp_2 = second
+        temp_3 = third
+        first = temp_1 + temp_2 + temp_3
+        second = temp_1
+        third = temp_2
+    i += 1
